754a0cbc3ddf26f410c706190080582d7154e0fe
Paweł Welfle <pawel.welfle@gmail.com>
2020-11-03 14:32:02 +0100

    update maven, add adnotation

--- lab2/src/main/java/ppkwu/lab2/APIController.java
+++ lab2/src/main/java/ppkwu/lab2/APIController.java
@@ -1,4 +1,12 @@
 package ppkwu.lab2;
 
+import org.springframework.web.bind.annotation.RestController;
+
+
+/**
+ * @author pawel.welfle
+ */
+
+@RestController
 public class APIController {
 }
--- lab2/src/main/java/ppkwu/lab2/Lab2Application.java
+++ lab2/src/main/java/ppkwu/lab2/Lab2Application.java
@@ -3,6 +3,10 @@ package ppkwu.lab2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author pawel.welfle
+ */
+
 @SpringBootApplication
 public class Lab2Application {
 
